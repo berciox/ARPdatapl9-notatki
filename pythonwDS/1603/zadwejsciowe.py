0c30b717f1c09edfe99849901911344ec3268e91
Mateusz <aaaccccccccccaa@gmail.com>
2023-03-16 18:19:34 +0100

    dodano skrypt

---
+++ pythonwDS/1603/zadwejsciowe.py
@@ -0,0 +1,17 @@
+# Napisz funkcję, która przyjmuja łańcuch znakowy
+#     (dla ułtawienia: same małe litery)
+# Przykładowo: alamakotaakotmapierdolca
+# Funkcja ma zwrócić słownik (return), który zawiera informacje w postaci:
+#     Klucz - litera
+#     Wartość - ilość wystąpień litery w tekście
+# Przykładowo: dla klucza "l" wartości to 2
+
+fraza = "alamakotaakotmapierdolca"
+slownik ={}
+
+def lancuch_znakowy():
+    for c in fraza:
+        if c not in slownik:
+            slownik[c] = fraza.count(c)
+    return slownik
+print(lancuch_znakowy())
